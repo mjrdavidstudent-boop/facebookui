aed541eb619cc2ff5485040ebe73d65a8a689c9a
mjrdavidstudent-boop <mjrdavid.student@ua.edu.ph>
2025-11-05 09:09:15 +0800

    URLS

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [content, setContent] = useState("");
   const [imageUrl, setImageUrl] = useState("");
 
-  const backendUrl = "https://facebookui-0n71.onrender.com";
+  const backendUrl = "https://facebookui-0n71.onrender.com/api/posts";
 
   // Fetch posts
   useEffect(() => {
